73009787985cd42cf7e26d02144099c1eff918ec
Chaion Saha <chaionsaha00@gmail.com>
2023-12-24 02:58:18 +0600

    fixed background image issue at search page

--- src/Pages/Shared/HeroSection.jsx
+++ src/Pages/Shared/HeroSection.jsx
@@ -25,42 +25,48 @@ const HeroSection = ({ imgLink, setLoading }) => {
 	};
 
 	return (
-		<div
-			style={{ background: `url(${imgLink})` }}
-			className='min-h-[75vh] flex justify-center items-center px-5 lg:px-0'
-		>
-			<div className='container relative flex flex-col p-5 lg:p-10 lg:py-12 rounded-xl bg-base-100'>
-				<div className='relative w-full rounded-xl'>
-					<label
-						htmlFor='search'
-						className={`absolute left-[2%] top-1/2 duration-150 z-[10] ${
-							isActive
-								? '-translate-y-[110%] text-sm text-gray-600'
-								: '-translate-y-1/2 text-xl text-black'
-						} `}
-					>
-						Location/Tour
-					</label>
-					<input
-						id='search'
-						{...register('search', { required: true })}
-						onFocus={() => setIsActive(true)}
-						onBlur={() => {
-							if (watch('search')) setIsActive(true);
-							else setIsActive(false);
-						}}
-						type='text'
-						className='w-full pt-5  input input-primary text-black font-medium input-lg text-xl focus:outline-none z-[100]'
-					/>
-				</div>
-				<button
-					onClick={() => handleSubmit(handleSearch)()}
-					className='lg:absolute self-center bottom-0 lg:px-16 px-10 py-3 lg:py-5 font-semibold text-white lg:-translate-x-1/2 active:scale-[0.9] duration-150 lg:translate-y-1/2 bg-primary rounded-xl left-1/2 mt-3 lg:mt-0'
+		<>
+			{imgLink && (
+				<div
+					style={{
+						background: `url(${imgLink})`,
+					}}
+					className='min-h-[75vh] bgImg flex justify-center items-center px-5 lg:px-0'
 				>
-					Search
-				</button>
-			</div>
-		</div>
+					<div className='container relative flex flex-col p-5 lg:p-10 lg:py-12 rounded-xl bg-base-100'>
+						<div className='relative w-full rounded-xl'>
+							<label
+								htmlFor='search'
+								className={`absolute left-[2%] top-1/2 duration-150 z-[10] ${
+									isActive
+										? '-translate-y-[110%] text-sm text-gray-600'
+										: '-translate-y-1/2 text-xl text-black'
+								} `}
+							>
+								Location/Tour
+							</label>
+							<input
+								id='search'
+								{...register('search', { required: true })}
+								onFocus={() => setIsActive(true)}
+								onBlur={() => {
+									if (watch('search')) setIsActive(true);
+									else setIsActive(false);
+								}}
+								type='text'
+								className='w-full pt-5  input input-primary text-black font-medium input-lg text-xl focus:outline-none z-[100]'
+							/>
+						</div>
+						<button
+							onClick={() => handleSubmit(handleSearch)()}
+							className='lg:absolute self-center bottom-0 lg:px-16 px-10 py-3 lg:py-5 font-semibold text-white lg:-translate-x-1/2 active:scale-[0.9] duration-150 lg:translate-y-1/2 bg-primary rounded-xl left-1/2 mt-3 lg:mt-0'
+						>
+							Search
+						</button>
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 
